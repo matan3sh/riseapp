b4f7d6f938aee1c60cce6cac47137de3a1e0246f
Matan <matan3sh@gmail.com>
2024-05-13 20:07:02 +0300

    Adding Clerk Hono Middleware to API Requests

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,33 +1,22 @@
-import { zValidator } from '@hono/zod-validator'
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
-import { z } from 'zod'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app
-  .get('/hello', (c) => {
-    return c.json({
-      message: 'Hello Next.js!',
-    })
+app.get('/hello', clerkMiddleware(), (c) => {
+  const auth = getAuth(c)
+
+  if (!auth?.userId) {
+    return c.json({ error: 'Unauthorized' })
+  }
+
+  return c.json({
+    message: 'Hello Next.js!',
   })
-  .get(
-    '/hello/:test',
-    zValidator(
-      'param',
-      z.object({
-        test: z.string(),
-      })
-    ),
-    (c) => {
-      const { test } = c.req.valid('param')
-      return c.json({
-        message: `Hello Next.js! ${test}`,
-      })
-    }
-  )
+})
 
 export const GET = handle(app)
 export const POST = handle(app)
