c91e99ab1562d7b0c3642f4e3b9ec531a9bdc25f
Matan <matan3sh@gmail.com>
2024-05-13 19:57:13 +0300

    Install Hono and Zod

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,5 +1,7 @@
+import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+import { z } from 'zod'
 
 export const runtime = 'edge'
 
@@ -11,12 +13,21 @@ app
       message: 'Hello Next.js!',
     })
   })
-  .get('/hello/:test', (c) => {
-    const test = c.req.param('test')
-    return c.json({
-      message: `Hello Next.js! ${test}`,
-    })
-  })
+  .get(
+    '/hello/:test',
+    zValidator(
+      'param',
+      z.object({
+        test: z.string(),
+      })
+    ),
+    (c) => {
+      const { test } = c.req.valid('param')
+      return c.json({
+        message: `Hello Next.js! ${test}`,
+      })
+    }
+  )
 
 export const GET = handle(app)
 export const POST = handle(app)
