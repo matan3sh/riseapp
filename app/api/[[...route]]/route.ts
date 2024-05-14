53189bcd67ec40d0d9e38636f401a9e6bcbe4c41
Matan <matan3sh@gmail.com>
2024-05-14 12:29:00 +0300

    Continue with HonoJS

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,22 +1,15 @@
-import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+
+import authors from './authors'
+import books from './books'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app.get('/hello', clerkMiddleware(), (c) => {
-  const auth = getAuth(c)
-
-  if (!auth?.userId) {
-    return c.json({ error: 'Unauthorized' })
-  }
-
-  return c.json({
-    message: 'Hello Next.js!',
-  })
-})
+app.route('/authors', authors)
+app.route('/books', books)
 
 export const GET = handle(app)
 export const POST = handle(app)
