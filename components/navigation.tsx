186760a822d6861e4fa5075fe3e326709bca29d3
Matan <matan3sh@gmail.com>
2024-05-17 12:07:36 +0300

    Header Navigation Implamantation

---
+++ components/navigation.tsx
@@ -0,0 +1,45 @@
+'use client'
+import NavButton from '@/components/nav-button'
+import { usePathname } from 'next/navigation'
+
+const routes = [
+  {
+    href: '/',
+    label: 'Overview',
+  },
+  {
+    href: '/transactions',
+    label: 'Transactions',
+  },
+  {
+    href: '/accounts',
+    label: 'Accounts',
+  },
+  {
+    href: '/categories',
+    label: 'Categories',
+  },
+  {
+    href: '/settings',
+    label: 'Settings',
+  },
+]
+
+const Navigation = () => {
+  const pathname = usePathname()
+
+  return (
+    <nav className="hidden lg:flex items-center gap-x-2 overflow-x-auto">
+      {routes.map((route) => (
+        <NavButton
+          key={route.href}
+          href={route.href}
+          label={route.label}
+          isActive={pathname === route.href}
+        />
+      ))}
+    </nav>
+  )
+}
+
+export default Navigation
