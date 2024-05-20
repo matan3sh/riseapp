eb10313feb5d1cd25b2e2f5aa05adadcabad171e
Matan <matan3sh@gmail.com>
2024-05-20 18:15:30 +0300

    Implement Welcome Message

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,8 @@
 import HeaderLogo from '@/components/header-logo'
 import Navigation from '@/components/navigation'
+import WelcomeMsg from '@/components/welcome-msg'
+import { ClerkLoaded, ClerkLoading, UserButton } from '@clerk/nextjs'
+import { Loader2 } from 'lucide-react'
 
 const Header = () => {
   return (
@@ -10,7 +13,14 @@ const Header = () => {
             <HeaderLogo />
             <Navigation />
           </div>
+          <ClerkLoaded>
+            <UserButton afterSignOutUrl="/" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="size-8 animate-spin text-slate-400" />
+          </ClerkLoading>
         </div>
+        <WelcomeMsg />
       </div>
     </header>
   )
