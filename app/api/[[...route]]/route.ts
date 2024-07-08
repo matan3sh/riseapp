cb6fbc932843c3d0f922b4cb49e55f843d9305ee
Matan <matan3sh@gmail.com>
2024-07-08 20:28:32 +0300

    Code Cleanup

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,18 +1,10 @@
 import { Hono } from 'hono'
-import { HTTPException } from 'hono/http-exception'
 import { handle } from 'hono/vercel'
 import accounts from './accounts'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
-
-app.onError((err, c) => {
-  if (err instanceof HTTPException) {
-    return err.getResponse()
-  }
-  return c.json({ error: 'Internal error' }, 500)
-})
 
 app.route('/accounts', accounts)
 
