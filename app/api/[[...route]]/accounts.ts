9cbf7d22a3fd0822a3c6ab932b07cff6d9ca0bf1
Matan <matan3sh@gmail.com>
2024-07-09 19:17:18 +0300

    Start Implementing Add Account Endpoint

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -4,22 +4,33 @@ import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 import { eq } from 'drizzle-orm'
 import { Hono } from 'hono'
 
-const app = new Hono().get('/', clerkMiddleware(), async (c) => {
-  const auth = getAuth(c)
+const app = new Hono()
+  .get('/', clerkMiddleware(), async (c) => {
+    const auth = getAuth(c)
 
-  if (!auth?.userId) {
-    return c.json({ error: 'Unauthorized' }, 401)
-  }
+    if (!auth?.userId) {
+      return c.json({ error: 'Unauthorized' }, 401)
+    }
 
-  const data = await db
-    .select({
-      id: accounts.id,
-      name: accounts.name,
-    })
-    .from(accounts)
-    .where(eq(accounts.userId, auth.userId))
+    const data = await db
+      .select({
+        id: accounts.id,
+        name: accounts.name,
+      })
+      .from(accounts)
+      .where(eq(accounts.userId, auth.userId))
 
-  return c.json({ data })
-})
+    return c.json({ data })
+  })
+
+  .post('/', clerkMiddleware(), async (c) => {
+    const auth = getAuth(c)
+
+    if (!auth?.userId) {
+      return c.json({ error: 'Unauthorized' }, 401)
+    }
+
+    return c.json({})
+  })
 
 export default app
