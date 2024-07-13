6248b6ba1449b380c4d9b6277ad431f377620698
Matan <matan3sh@gmail.com>
2024-07-13 10:25:05 +0300

    Finish Add Acoount Endpoint

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,6 +1,8 @@
 import { db } from '@/db/drizzle'
-import { accounts } from '@/db/schema'
+import { accounts, insertAccountSchema } from '@/db/schema'
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
+import { zValidator } from '@hono/zod-validator'
+import { createId } from '@paralleldrive/cuid2'
 import { eq } from 'drizzle-orm'
 import { Hono } from 'hono'
 
@@ -23,14 +25,34 @@ const app = new Hono()
     return c.json({ data })
   })
 
-  .post('/', clerkMiddleware(), async (c) => {
-    const auth = getAuth(c)
+  .post(
+    '/',
+    clerkMiddleware(),
+    zValidator(
+      'json',
+      insertAccountSchema.pick({
+        name: true,
+      })
+    ),
+    async (c) => {
+      const auth = getAuth(c)
+      const values = c.req.valid('json')
 
-    if (!auth?.userId) {
-      return c.json({ error: 'Unauthorized' }, 401)
+      if (!auth?.userId) {
+        return c.json({ error: 'Unauthorized' }, 401)
+      }
+
+      const [data] = await db
+        .insert(accounts)
+        .values({
+          id: createId(),
+          userId: auth.userId,
+          ...values,
+        })
+        .returning()
+
+      return c.json({ data })
     }
-
-    return c.json({})
-  })
+  )
 
 export default app
