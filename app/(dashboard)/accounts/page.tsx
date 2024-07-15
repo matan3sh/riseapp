e115096b3140ffb540d36dbda327656d429c32c6
Matan <matan3sh@gmail.com>
2024-07-15 19:17:47 +0300

    Add Data Table Mock Data

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,9 +1,20 @@
 'use client'
 
+import { columns, Payment } from '@/app/(dashboard)/accounts/columns'
+import { DataTable } from '@/components/data-table'
 import { Button } from '@/components/ui/button'
-import { Card, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { useNewAccount } from '@/features/accounts/hooks/use-new-account'
 import { Plus } from 'lucide-react'
+
+const data: Payment[] = [
+  {
+    id: '728ed52f',
+    amount: 100,
+    status: 'pending',
+    email: 'm@example.com',
+  },
+]
 
 const AccountsPage = () => {
   const newAccount = useNewAccount()
@@ -18,6 +29,9 @@ const AccountsPage = () => {
             Add new
           </Button>
         </CardHeader>
+        <CardContent>
+          <DataTable columns={columns} data={data} />
+        </CardContent>
       </Card>
     </div>
   )
