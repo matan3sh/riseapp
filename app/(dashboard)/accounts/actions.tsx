454e73d0ccd748bee4c9eb64bea8b8fd44632c6d
Matan <matan3sh@gmail.com>
2024-07-31 20:50:10 +0300

    Change Text From Transaction to Account

--- app/(dashboard)/accounts/actions.tsx
+++ app/(dashboard)/accounts/actions.tsx
@@ -21,7 +21,7 @@ export const Actions = ({ id }: Props) => {
   const { onOpen } = useOpenAccount()
   const [ConfirmDialog, confirm] = useConfirm(
     'Are you sure?',
-    'You are about to delete this transaction.'
+    'You are about to delete this account.'
   )
 
   const handleDelete = async () => {
