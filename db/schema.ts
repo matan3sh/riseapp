bf333ba996854df85bfec425ab0277887d2f2f51
Matan <matan3sh@gmail.com>
2024-08-03 19:16:28 +0300

    Update Schema with Transactions Table

--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,7 @@
-import { pgTable, text } from 'drizzle-orm/pg-core'
+import { relations } from 'drizzle-orm'
+import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
 import { createInsertSchema } from 'drizzle-zod'
+import { z } from 'zod'
 
 export const accounts = pgTable('accounts', {
   id: text('id').primaryKey(),
@@ -7,6 +9,10 @@ export const accounts = pgTable('accounts', {
   name: text('name').notNull(),
   userId: text('user_id').notNull(),
 })
+
+export const accountsRelations = relations(accounts, ({ many }) => ({
+  trancactions: many(trancactions),
+}))
 
 export const insertAccountSchema = createInsertSchema(accounts)
 
@@ -17,4 +23,39 @@ export const categories = pgTable('categories', {
   userId: text('user_id').notNull(),
 })
 
+export const categoriesRelations = relations(categories, ({ many }) => ({
+  trancactions: many(trancactions),
+}))
+
 export const insertCategorySchema = createInsertSchema(categories)
+
+export const trancactions = pgTable('transactions', {
+  id: text('id').primaryKey(),
+  amount: integer('amount').notNull(),
+  payee: text('payee').notNull(),
+  notes: text('notes'),
+  date: timestamp('date').notNull(),
+  accountId: text('account_id')
+    .references(() => accounts.id, {
+      onDelete: 'cascade',
+    })
+    .notNull(),
+  categoryId: text('category_id').references(() => categories.id, {
+    onDelete: 'set null',
+  }),
+})
+
+export const transactionsRelations = relations(trancactions, ({ one }) => ({
+  account: one(accounts, {
+    fields: [trancactions.accountId],
+    references: [accounts.id],
+  }),
+  categories: one(categories, {
+    fields: [trancactions.categoryId],
+    references: [categories.id],
+  }),
+}))
+
+export const insertTransactionSchema = createInsertSchema(trancactions, {
+  date: z.coerce.date(),
+})
