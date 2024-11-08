049b45251962f6424f4c371689ef469fb703beb8
Matan <matan3sh@gmail.com>
2024-11-08 09:53:08 +0200

    Open Edit Transaction if Transaction is Uncategorized

--- app/(dashboard)/transactions/category-column.tsx
+++ app/(dashboard)/transactions/category-column.tsx
@@ -1,4 +1,5 @@
 import { useOpenCategory } from '@/features/categories/hooks/use-open-category'
+import { useOpenTransaction } from '@/features/transactions/hooks/use-open-transaction'
 import { cn } from '@/lib/utils'
 import { TriangleAlert } from 'lucide-react'
 
@@ -8,11 +9,13 @@ type Props = {
   categoryId: string | null
 }
 
-export const CategoryColumn = ({ category, categoryId }: Props) => {
+export const CategoryColumn = ({ id, category, categoryId }: Props) => {
   const { onOpen: onOpenCategory } = useOpenCategory()
+  const { onOpen: onOpenTransaction } = useOpenTransaction()
 
   const onClick = () => {
     if (categoryId) onOpenCategory(categoryId)
+    else onOpenTransaction(id)
   }
 
   return (
