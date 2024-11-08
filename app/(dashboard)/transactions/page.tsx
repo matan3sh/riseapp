b5484c0b0f353e2c7cf418b1225b03fea22183f1
Matan <matan3sh@gmail.com>
2024-11-08 14:39:00 +0200

    Fix Filter By Payee

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -49,7 +49,7 @@ const TransactionsPage = () => {
         </CardHeader>
         <CardContent>
           <DataTable
-            filterKey="name"
+            filterKey="payee"
             columns={columns}
             data={transactions}
             onDelete={(row) => {
