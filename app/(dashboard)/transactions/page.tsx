116ad70ed9641b599fa14329c86cde7615c23874
Matan <matan3sh@gmail.com>
2024-12-03 11:17:07 +0200

    Finish Implementing Import Feature

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -1,17 +1,25 @@
 'use client'
 
-import { columns } from '@/app/(dashboard)/transactions/columns'
-import { ImportCard } from '@/app/(dashboard)/transactions/import-card'
-import UploadButton from '@/app/(dashboard)/transactions/upload-button'
+import { Loader2, Plus } from 'lucide-react'
+import { useState } from 'react'
+import { toast } from 'sonner'
+
+import { useBulkCreateTransactions } from '@/features/transactions/api/use-bulk-create-transactions'
+import { useBulkDeleteTransactions } from '@/features/transactions/api/use-bulk-delete-transactions'
+import { useGetTransactions } from '@/features/transactions/api/use-get-transactions'
+import { useNewTransaction } from '@/features/transactions/hooks/use-new-transaction'
+
+import { useSelectAccount } from '@/features/accounts/hooks/use-select-account'
+
 import { DataTable } from '@/components/data-table'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Skeleton } from '@/components/ui/skeleton'
-import { useBulkDeleteTransactions } from '@/features/transactions/api/use-bulk-delete-transactions'
-import { useGetTransactions } from '@/features/transactions/api/use-get-transactions'
-import { useNewTransaction } from '@/features/transactions/hooks/use-new-transaction'
-import { Loader2, Plus } from 'lucide-react'
-import { useState } from 'react'
+import { transactions as transactionSchema } from '@/db/schema'
+
+import { columns } from './columns'
+import { ImportCard } from './import-card'
+import UploadButton from './upload-button'
 
 enum VARIANTS {
   LIST = 'LIST',
@@ -21,22 +29,15 @@ enum VARIANTS {
 const INITIAL_IMPORT_RESULTS = {
   data: [],
   errors: [],
-  meta: [],
+  meta: {},
 }
 
 const TransactionsPage = () => {
+  const [AccountDialog, confirm] = useSelectAccount()
   const [variant, setVariant] = useState<VARIANTS>(VARIANTS.LIST)
   const [importResults, setImportResults] = useState(INITIAL_IMPORT_RESULTS)
 
-  const newTransaction = useNewTransaction()
-  const deleteTransactions = useBulkDeleteTransactions()
-  const transactionsQuery = useGetTransactions()
-  const transactions = transactionsQuery.data || []
-
-  const isDisabled = transactionsQuery.isLoading || deleteTransactions.isPending
-
   const onUpload = (results: typeof INITIAL_IMPORT_RESULTS) => {
-    console.log({ results })
     setImportResults(results)
     setVariant(VARIANTS.IMPORT)
   }
@@ -44,6 +45,35 @@ const TransactionsPage = () => {
   const onCancelImport = () => {
     setImportResults(INITIAL_IMPORT_RESULTS)
     setVariant(VARIANTS.LIST)
+  }
+
+  const newTransaction = useNewTransaction()
+  const createTransactions = useBulkCreateTransactions()
+  const deleteTransactions = useBulkDeleteTransactions()
+  const transactionsQuery = useGetTransactions()
+  const transactions = transactionsQuery.data || []
+
+  const isDisabled = transactionsQuery.isLoading || deleteTransactions.isPending
+
+  const onSubmitImport = async (
+    values: (typeof transactionSchema.$inferInsert)[]
+  ) => {
+    const accountId = await confirm()
+
+    if (!accountId) {
+      return toast.error('Please select an account to continue.')
+    }
+
+    const data = values.map((value) => ({
+      ...value,
+      accountId: accountId as string,
+    }))
+
+    createTransactions.mutate(data, {
+      onSuccess: () => {
+        onCancelImport()
+      },
+    })
   }
 
   if (transactionsQuery.isLoading) {
@@ -66,10 +96,11 @@ const TransactionsPage = () => {
   if (variant === VARIANTS.IMPORT) {
     return (
       <>
+        <AccountDialog />
         <ImportCard
           data={importResults.data}
           onCancel={onCancelImport}
-          onSubmit={() => {}}
+          onSubmit={onSubmitImport}
         />
       </>
     )
@@ -80,13 +111,13 @@ const TransactionsPage = () => {
       <Card className="border-none drop-shadow-sm">
         <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
           <CardTitle className="text-xl line-clamp-1">
-            Transactions history
+            Transaction History
           </CardTitle>
-          <div className="flex flex-col lg:flex-row gap-y-2 items-center gap-2">
+          <div className="flex flex-col lg:flex-row gap-y-2 items-center gap-x-2">
             <Button
-              className="w-full lg:w-auto"
               onClick={newTransaction.onOpen}
               size="sm"
+              className="w-full lg:w-auto"
             >
               <Plus className="size-4 mr-2" />
               Add new
